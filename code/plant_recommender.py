983af5c24b3e35089002f916435b3b532bdc5815
josephh867 <josephh867@gmail.com>
2021-08-18 08:45:42 -0600

    Working on the streamlit app

--- code/plant_recommender.py
+++ code/plant_recommender.py
@@ -2,6 +2,12 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+import sqlite3
+
+# Clustering and other modeling Imports
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import silhouette_score
+from sklearn.cluster import SpectralClustering
 
 st.title('Plant Recommender App')
 st.write("""
@@ -10,84 +16,159 @@ Created by Joseph Hicks
 """)
 st.markdown('[GitHub](https://github.com/josephh867)')
 
+# Import data
+# Grab user input on what the file will be called
+output_name = input('Enter desired filename for the cleaned data:')
 
+# Read in the data into a dataframe
+con = sqlite3.connect('../datasets/usdadb_new.sqlite3')
+df = pd.read_sql_query('''
+                       SELECT *
+                       FROM usda
+                       WHERE Temperature_Minimum_F IS NOT ''
+                       ''', con
+                       )
 
-# Ask user if they would like to run the cleaning steps to initialize
-st.write("""To run this app locally, you will need to perform data cleaning first so the model will run correctly.
-To do this, hit the button below:
+# Close the connection
+con.close()
+
+# Sorting and storing desired features
+features = ['id', 'Scientific_Name_x', 'Category', 'Family', 'Growth_Habit', 'Native_Status',
+           'Active_Growth_Period', 'Fall_Conspicuous', 'Fire_Resistance', 'Flower_Color',
+           'Flower_Conspicuous', 'Fruit_Conspicuous', 'Growth_Rate', 'Lifespan', 'Toxicity',
+           'Drought_Tolerance', 'Hedge_Tolerance', 'Moisture_Use', 'pH_Minimum', 'pH_Maximum',
+           'Salinity_Tolerance', 'Shade_Tolerance', 'Temperature_Minimum_F', 'Bloom_Period'
+           ]
+
+df = df[features]
+
+categorical_features = ['Category', 'Family', 'Growth_Habit', 'Native_Status',
+                        'Active_Growth_Period', 'Fall_Conspicuous', 'Flower_Color',
+                        'Flower_Conspicuous', 'Fruit_Conspicuous', 'Bloom_Period', 'Fire_Resistance'
+                        ]
+
+ordinal_features = ['Toxicity', 'Drought_Tolerance', 'Hedge_Tolerance',
+                   'Moisture_Use', 'Salinity_Tolerance', 'Shade_Tolerance', 'Growth_Rate', 'Lifespan'
+                   ]
+
+other_features = ['id', 'Scientific_Name_x', 'pH_Minimum', 'pH_Maximum',
+                 'Temperature_Minimum_F'
+                 ]
+
+# Instaniating the SpectralClustering model
+spec = SpectralClustering(gamma=0.5, n_clusters=5, n_init=5, n_jobs=-1)
+
+# Recommender Submission Form
+st.write("""Fill out the form below and hit SUBMIT to make a query. Please note that you do not have to fill out every field.
 """)
-clean = st.button('Perform Data Cleaning')
+with st.form('recommender'):
+    # Create an empty dictionary to gather values for the dummy entry
+    dummy = {}
+    dummy['id'] = 42
+    dummy['Scientific_Name_x'] = 'sample'
 
-if clean:
-    # Grab user input on what the file will be called
-    output_name = st.text_input('Enter desired filename for the cleaned data.')
+    # Inputs for simple user-chosen features
+    dummy['Lifespan'] = st.selectbox('Choose a desired plant lifespan',
+                                     ('Short', 'Moderate', 'Long'))
+    dummy['Drought_Tolerance'] = st.selectbox('Choose the desired drought tolerance',
+                                     ('Low', 'Medium', 'High'))
+    dummy['Moisture'] = st.selectbox('Enter the expected moisture use',
+                                     ('Low', 'Medium', 'High'))
+    dummy['Hedge_Tolerance'] = st.selectbox('Enter the expected moisture use',
+                                     ('Low', 'Medium', 'High'))
+    dummy['Shade_Tolerance'] = st.selectbox('How shade tolerant will your plants need to be?',
+                                     ('Intolerant', 'Intermediate', 'Tolerant'))
+    dummy['Salinity_Tolerance'] = st.selectbox('How salt tolerant will your plants need to be?',
+                                     ('None', 'Low', 'Medium', 'High'))
+    dummy['Growth_Rate'] = st.selectbox('Select Desired Growth Rate',
+                                        ('Slow', 'Moderate', 'Rapid'))
+    dummy['Temperature_Minimum_F'] = st.number_input('Enter the coldest expected temperature for your area in F')
+    dummy['pH_Minimum'] = st.number_input('Enter the minimum soil pH')
+    dummy['pH_Maximum'] = st.number_input('Enter the maximum soil pH')
+    dummy['Flower_Conspicuous'] = st.radio('Would you prefer a plant with showy flowers?',
+                                           ('Yes', 'No'))
+    dummy['Flower_Color'] = st.selectbox('What flower color would you prefer?',
+                                         ('Yellow', 'Red', 'Purple', 'Brown', 'Blue', 'Green', 'White', 'Orange'))
+    dummy['Fall_Conspicuous'] = st.radio('Would you prefer a plant with bright autumn colors?',
+                                           ('Yes', 'No'))
+    dummy['Fire_Resistance'] = st.radio('Would you prefer a plant with fire resistance?',
+                                           ('Yes', 'No'))
+    dummy['Fruit_Conspicuous'] = st.radio('Would you prefer a plant with conspicuous and/or edible fruit?',
+                                           ('Yes', 'No'))
+    dummy['Flower_Conspicuous'] = st.radio('Would you prefer a plant with showy flowers?',
+                                           ('Yes', 'No'))
+    neighbors = st.slider('Select the number of results you would like to recieve',
+                          value=5,
+                          min_value=1,
+                          max_value=50
+                          )
 
-    # Read in the data into a dataframe
-    con = sqlite3.connect('../datasets/usdadb_new.sqlite3')
-    df = pd.read_sql_query('''
-                           SELECT *
-                           FROM usda
-                           WHERE Temperature_Minimum_F IS NOT ''
-                           ''', con
-                           )
+    # Submit the form and start the modeling process
+    submitted = st.form_submit_button('SUBMIT')
+    if submitted:
+        # Fill in the other columns with dummy values if they are not specified
+        for col in features:
+            if col not in dummy.keys():
+                dummy[col] = ''
 
-    # Close the connection
-    con.close()
+        # Append the dummy entry to the overall dataset
+        df_d = pd.DataFrame(dummy, index=[0])
+        data = df.append(df_d)
 
-    # Sorting and storing desired features
-    features = ['id', 'Scientific_Name_x', 'Category', 'Family', 'Growth_Habit', 'Native_Status',
-               'Active_Growth_Period', 'Fall_Conspicuous', 'Fire_Resistance', 'Flower_Color',
-               'Flower_Conspicuous', 'Fruit_Conspicuous', 'Growth_Rate', 'Lifespan', 'Toxicity',
-               'Drought_Tolerance', 'Hedge_Tolerance', 'Moisture_Use', 'pH_Minimum', 'pH_Maximum',
-               'Salinity_Tolerance', 'Shade_Tolerance', 'Temperature_Minimum_F', 'Bloom_Period'
-               ]
+        # Drop the label columns for now, recombine with the data later
+        labels = data[['id', 'Scientific_Name_x']]
+        data.drop(columns=['id', 'Scientific_Name_x'], inplace=True)
 
-    df = df[features]
+        # Perform data cleaning
+        # Map the ordinal features
+        ord_dict = {
+           # Toxicity
+           'None':0, 'Slight':1, 'Moderate':2, 'Severe':3,
 
-    categorical_features = ['Category', 'Family', 'Growth_Habit', 'Native_Status',
-                            'Active_Growth_Period', 'Fall_Conspicuous', 'Flower_Color',
-                            'Flower_Conspicuous', 'Fruit_Conspicuous', 'Bloom_Period', 'Fire_Resistance'
-                            ]
+           # Tolerances other than shade tolerance
+           'None':0, 'Low':1, 'Medium':2, 'High':3,
 
-    ordinal_features = ['Toxicity', 'Drought_Tolerance', 'Hedge_Tolerance',
-                       'Moisture_Use', 'Salinity_Tolerance', 'Shade_Tolerance', 'Growth_Rate', 'Lifespan'
-                       ]
+           # Shade Tolerance
+           'Intolerant':0, 'Intermediate':1, 'Tolerant':2,
 
-    other_features = ['id', 'Scientific_Name_x', 'pH_Minimum', 'pH_Maximum',
-                     'Temperature_Minimum_F'
-                     ]
+           # Unique keys for Growth Rate and Lifespan
+           'Slow':1, 'Short':1, 'Long':3, 'Rapid':3
+        }
 
-    # Map the ordinal features
-    ord_dict = {
-       # Toxicity
-       'None':0, 'Slight':1, 'Moderate':2, 'Severe':3,
+        def ordinal_mapper(df, ord_features):
 
-       # Tolerances other than shade tolerance
-       'None':0, 'Low':1, 'Medium':2, 'High':3,
+            # Fill empty strings with -1
+            df[ord_features] = df[ord_features].replace('', -1)
 
-       # Shade Tolerance
-       'Intolerant':0, 'Intermediate':1, 'Tolerant':2,
+            # Get values from the dictionary using the get method
+            df = df.applymap(lambda x: ord_dict.get(x,x))
+            return df
 
-       # Unique keys for Growth Rate and Lifespan
-       'Slow':1, 'Short':1, 'Long':3, 'Rapid':3
-    }
+        data = ordinal_mapper(data, ordinal_features)
 
-    def ordinal_mapper(df, ord_features):
+        # Dummify the nominal features
+        data = pd.get_dummies(data, columns=categorical_features, drop_first=True, dummy_na=True)
 
-        # Fill empty strings with 0
-        df[ord_features] = df[ord_features].replace('', -1)
+        # Now scale the data and perform clustering
+        sc = StandardScaler()
+        data_sc = sc.fit_transform(data)
+        spec.fit_predict(data_sc)
 
-        # Get values from the dictionary using the get method
-        df = df.applymap(lambda x: ord_dict.get(x,x))
-        return df
+        # Store cluster predictions and dummy entry cluster
+        data['cluster'] = spec.labels_
+        out_cluster = spec.labels_[-1]
 
-    df = ordinal_mapper(df, ordinal_features)
+        # Recombine the data with the label features
+        data = pd.concat([data, labels], axis=1)
 
-    # Dummify the nominal features
-    df = pd.get_dummies(df, columns=categorical_features, drop_first=True, dummy_na=True)
+        # Filter down to the dummy entry and its nearest neighbors
+        output = data.loc[data['cluster'] == out_cluster]
 
-    # Save the dataframe to csv with specified filename
-    df.to_csv('../datasets/'+output_name+'.csv',index=False)
+        # Sample from the filtered dataset
+        results = output[['id', 'Scientific_Name_x']].sample(neighbors)
+
+        # Display results
+        results
 
 
 
