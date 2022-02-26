6aec19c71c2daf7b91b5c68aea084052351f092a
josephh867 <josephh867@gmail.com>
2022-02-25 18:37:30 -0700

    Fixing database issue with raw github link

--- code/.ipynb_checkpoints/plant_recommender_st-checkpoint.py
+++ code/.ipynb_checkpoints/plant_recommender_st-checkpoint.py
@@ -23,8 +23,8 @@ st.markdown('[GitHub](https://github.com/josephh867)')
 
 # Import data
 
-# Read in the data into a dataframe
-df = pd.read_csv('https://github.com/josephh867/plant-recommender/blob/main/datasets/cleaned-data.csv')
+# Read in the data into a dataframe from raw github csv
+df = pd.read_csv('https://raw.githubusercontent.com/josephh867/plant-recommender/main/datasets/cleaned-data.csv')
 
 # Everything commentted out below is redundant now
 # con = sqlite3.connect('../datasets/usdadb_new.sqlite3')
